4bc76b89a0312449cee06d32d7f9a9f7ee8a7a8e
LuizLins <luizmagao@gmail.com>
2025-10-10 21:42:23 -0300

    WIP: NavBar e um menulateral

---
+++ src/components/navBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <Menubar :model="items" style="background: gray">
+    <template #start>
+      <Image
+        src="https://app.cajutec.com.br/assets/cajutec-color.cb5c64a4.png"
+        alt="Image"
+        width="20"
+      />
+    </template>
+  </Menubar>
+</template>
+
+<script setup>
+import Menubar from "primevue/menubar";
+import Image from "primevue/image";
+import { ref } from "vue";
+
+const items = ref([
+  {
+    label: "Home",
+    icon: "pi pi-home",
+  },
+  {
+    label: "Agendas",
+    icon: "pi pi-search",
+    items: [
+      {
+        label: "Calendário",
+        icon: "pi pi-bolt",
+      },
+      {
+        label: "Datas comemorativas",
+        icon: "pi pi-server",
+      },
+      {
+        label: "Feriados",
+        icon: "pi pi-pencil",
+      },
+    ],
+  },
+  {
+    label: "Contato",
+    icon: "pi pi-envelope",
+  },
+]);
+</script>
+
+<style lang="scss" scoped></style>
--- src/layouts/layoutDefault.vue
+++ src/layouts/layoutDefault.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    Layout
-    <RouterView />
+    <nav-bar />
+  </div>
+  <div style="display: flex">
+    <div>
+      <div class="card flex justify-center">
+        <Menu :model="items" />
+      </div>
+    </div>
+    <div><RouterView /></div>
   </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
+import navBar from "@/components/navBar.vue";
+import Menu from "primevue/menu";
+
+import { ref } from "vue";
+
+const items = ref([
+  { label: "New", icon: "pi pi-plus" },
+  { label: "Search", icon: "pi pi-search" },
+]);
 </script>
 
 <style lang="scss" scoped></style>
